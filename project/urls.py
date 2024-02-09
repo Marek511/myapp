16e3a291c83aa6d0149e7d445524ee640083ad75
Roman Dryndik <dryndikroman@gmail.com>
2024-02-09 18:14:47 +0100

    Fix templates

--- project/urls.py
+++ project/urls.py
@@ -2,10 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from quotes.views import main
+
 
 urlpatterns = [
+    path('', include('quotes.urls')),
     path('admin/', admin.site.urls),
-    path('quotes/', include('quotes.urls')),
     path('users/', include('users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
